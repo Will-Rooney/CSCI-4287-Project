5b3402d210d5f7457d862bc0312737730a35b87e
Will Rooney <william.rooney@ucdenver.edu>
2017-04-24 17:29:28 -0600

    Formatting Fix

--- PathFinding/DStarSearch_Simulation.ino
+++ PathFinding/DStarSearch_Simulation.ino
@@ -89,57 +89,57 @@ byte getReorientAct() {
 
 /***** SETUP *****/
 void setup() {
-	Serial.begin(9600);
-	pinMode(ledPin, OUTPUT);
-	Serial.println("Init");
-
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  Serial.println("Init");
+  
   // Initialize pathfinding and compute initial path
-	search.init();
-	search.computeShortestPath();
-
+  search.init();
+  search.computeShortestPath();
+  
   // SIMULATION - Initialize sim-car
   // Start position: (0, 0)
-	car.row = 0;
-	car.col = 0;
+  car.row = 0;
+  car.col = 0;
   car.orientation = 1; // RIGHT
 }
 
 /***** LOOP *****/
 void loop() {
-	if (GOAL) { // GOAL REACHED
-		digitalWrite(ledPin, HIGH);
-		delay(500);
-		digitalWrite(ledPin, LOW);
-		delay(500);
-	}
-	else if (FAIL) { // CANNOT REACH GOAL
-		digitalWrite(ledPin, HIGH);
-		delay(50);
-		digitalWrite(ledPin, LOW);
-		delay(50);
-	}
-	else if (!GOAL) { // MAIN LOOP - Navigate to the goal
-		digitalWrite(ledPin, HIGH);
-		delay(1000);
-		stepCount++; // The number of steps taken since the last action was completed
-
-		// if we've completed the action
-		if (stepCount == actionCount) // This check will be done by checking sensor data to verify car's position
-			ACTION_COMPLETE = true;
-
-		// Get the next action to take and how many blocks to take it
-		if (ACTION_COMPLETE) {
-			search.getNextAction(action, actionCount); // Get the next action and the number of times to take that action
+  if (GOAL) { // GOAL REACHED
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    delay(500);
+  }
+  else if (FAIL) { // CANNOT REACH GOAL
+    digitalWrite(ledPin, HIGH);
+    delay(50);
+    digitalWrite(ledPin, LOW);
+    delay(50);
+  }
+  else if (!GOAL) { // MAIN LOOP - Navigate to the goal
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+    stepCount++; // The number of steps taken since the last action was completed
+  
+    // if we've completed the action
+    if (stepCount == actionCount) // This check will be done by checking sensor data to verify car's position
+      ACTION_COMPLETE = true;
+  
+    // Get the next action to take and how many blocks to take it
+    if (ACTION_COMPLETE) {
+      search.getNextAction(action, actionCount); // Get the next action and the number of times to take that action
       ACTION_COMPLETE = false;  // New action -> reset ACTION_COMPLETE
       stepCount = 0;            // SIMULATION New action -> reset stepCount
-			if (action == BYTE_INF) { // BYTE_INF is a custom globally defined variable part of my pathfinding code: BYTE_INF == 255
-				FAIL = true;
-				Serial.println("Unable to reach the goal!");
-				return;
-			}
-
+      if (action == BYTE_INF) { // BYTE_INF is a custom globally defined variable part of my pathfinding code: BYTE_INF == 255
+        FAIL = true;
+        Serial.println("Unable to reach the goal!");
+        return;
+      }
+      
       // Determine if the car needs to reorient for the next action
-      orientAction = getReorientAction();
+      orientAction = getReorientAct();
       if (orientAction != 0) { // Need to reorient
         // Reorient the car
         if (orientAction == 1) {
@@ -155,68 +155,68 @@ void loop() {
           // Turn the car around (180 degrees)
         }
       }
-
+      
       /* FOR TESTING PURPOSES -  NOT NEEDED IN FINAL IMPLEMENTATION */
-			Serial.println();
-			Serial.print("New action: ");
-			switch (action) {
-			case 0: // up
-				Serial.print("'UP'");
-				break;
-			case 1: // right
-				Serial.print("'RIGHT'");
-				break;
-			case 2: // down
-				Serial.print("'DOWN'");
-				break;
-			case 3: // left
-				Serial.print("'LEFT'");
-				break;
-			}
-			Serial.print("\n\tSteps to take: ");
-			Serial.print(actionCount);
-			Serial.println();
-			Serial.println();
-		}
-
+      Serial.println();
+      Serial.print("New action: ");
+      switch (action) {
+      case 0: // up
+        Serial.print("'UP'");
+        break;
+      case 1: // right
+        Serial.print("'RIGHT'");
+        break;
+      case 2: // down
+        Serial.print("'DOWN'");
+        break;
+      case 3: // left
+        Serial.print("'LEFT'");
+        break;
+      }
+      Serial.print("\n\tSteps to take: ");
+      Serial.print(actionCount);
+      Serial.println();
+      Serial.println();
+    }
+  
     /* check for obstacles */
     
-		/* Obstacle detected? - SIMULATE AN OBSTACLE POPPING UP */
-		if (stepCount == stepsToObstacle) {
-			stepsToObstacle++; // Increase the number of steps until we throw in an obstacle
+  	/* Obstacle detected? - SIMULATE AN OBSTACLE POPPING UP */
+  	if (stepCount == stepsToObstacle) {
+      stepsToObstacle++; // Increase the number of steps until we throw in an obstacle
       
-			stepCount = 0;  // Reset step count - new action pending
-
-			/* FOR TESTING PURPOSES -  NOT NEEDED IN FINAL IMPLEMENTATION */
+      stepCount = 0;  // Reset step count - new action pending
+      
+      /* FOR TESTING PURPOSES -  NOT NEEDED IN FINAL IMPLEMENTATION */
       /* The car will update its location based on motor control and verified with sensor data */
-			Serial.print("\nObstacle detected at (");
-			switch (action) {
-			case 0: // up
-				Serial.print(car.row - 1);
-				Serial.print(", ");
-				Serial.print(car.col);
-				Serial.print(")\n");
-				break;
-			case 1: // right
-				Serial.print(car.row);
-				Serial.print(", ");
-				Serial.print(car.col + 1);
-				Serial.print(")\n");
-				break;
-			case 2: // down
-				Serial.print(car.row + 1);
-				Serial.print(", ");
-				Serial.print(car.col);
-				Serial.print(")\n");
-				break;
-			case 3: // left
-				Serial.print(car.row);
-				Serial.print(", ");
-				Serial.print(car.col - 1);
-				Serial.print(")\n");
-				break;
-			}
-
+      Serial.print("\nObstacle detected at (");
+      switch (action) {
+      case 0: // up
+        Serial.print(car.row - 1);
+        Serial.print(", ");
+        Serial.print(car.col);
+        Serial.print(")\n");
+        break;
+      case 1: // right
+        Serial.print(car.row);
+        Serial.print(", ");
+        Serial.print(car.col + 1);
+        Serial.print(")\n");
+        break;
+      case 2: // down
+        Serial.print(car.row + 1);
+        Serial.print(", ");
+        Serial.print(car.col);
+        Serial.print(")\n");
+        break;
+      case 3: // left
+        Serial.print(car.row);
+        Serial.print(", ");
+        Serial.print(car.col - 1);
+        Serial.print(")\n");
+        break;
+      }
+      
       /* recomputeShortestPath(byte row, byte col, byte &action, byte &actionCount)
        *  pre:
        *    (row, col) == Cars current position
@@ -230,15 +230,15 @@ void loop() {
        *      We do not need to return to the previous node, a new action will be retreived
        *      at the beginning of the loop()
        */
-			search.recomputeShortestPath(car.row, car.col, action, actionCount);
-
+      search.recomputeShortestPath(car.row, car.col, action, actionCount);
+      
       /* Check if we need to return to the previous node after recomputing the path */
-			if (actionCount == 0) // We did move from the previous node, therefore we do not need to return 
-				ACTION_COMPLETE = true;
-			else { // We started to move to the next node and were blocked, therefore we DO need to return to previous node
-				ACTION_COMPLETE = false;
+      if (actionCount == 0) // We did move from the previous node, therefore we do not need to return 
+        ACTION_COMPLETE = true;
+      else { // We started to move to the next node and were blocked, therefore we DO need to return to previous node
+        ACTION_COMPLETE = false;
         // Reorient the car
-        orientAction = getReorientAction();
+        orientAction = getReorientAct();
         if (orientAction != 0) { // Need to reorient
           // Reorient the car
           if (orientAction == 1) {
@@ -254,44 +254,44 @@ void loop() {
             // Turn the car around (180 degrees)
           }
         }
-			}
-			return;
-		}
-
-		/* Move car based on 'action' and 'actionCount', then update car's position */
-
+      }
+      return;
+  	}
+  
+  	/* Move car based on 'action' and 'actionCount', then update car's position */
+  
     /* Check distance sensors and center the car if needed */
    
     /* FOR TESTING PURPOSES -  NOT NEEDED IN FINAL IMPLEMENTATION */
     /* The car will update its location based on motor control and verified with sensor data */
-		Serial.print("Performing action: ");
-		switch (action) {
-		case 0: // up
-			Serial.print("'UP'\n");
-			car.row--;
-			break;
-		case 1: // right
-			Serial.print("'RIGHT'\n");
-			car.col++;
-			break;
-		case 2: // down
-			Serial.print("'DOWN'\n");
-			car.row++;
-			break;
-		case 3: // left
-			Serial.print("'LEFT'\n");
-			car.col--;
-			break;
-		}
-
-		/* If we have finished performing the action - Are we at the goal? */
+  	Serial.print("Performing action: ");
+  	switch (action) {
+  	case 0: // up
+      Serial.print("'UP'\n");
+      car.row--;
+      break;
+  	case 1: // right
+      Serial.print("'RIGHT'\n");
+      car.col++;
+      break;
+  	case 2: // down
+      Serial.print("'DOWN'\n");
+      car.row++;
+      break;
+  	case 3: // left
+      Serial.print("'LEFT'\n");
+      car.col--;
+      break;
+  	}
+  
+  	/* If we have finished performing the action - Are we at the goal? */
     /* Goal position (row,col): (9, 9) */
-		if (car.row == 9 && car.col == 9) {
-			GOAL = true;
-			Serial.println("Goal!");
-		}
-
-		digitalWrite(ledPin, LOW);
-		delay(1000);
-	}
+  	if (car.row == 9 && car.col == 9) {
+      GOAL = true;
+      Serial.println("Goal!");
+  	}
+  
+  	digitalWrite(ledPin, LOW);
+  	delay(1000);
+  }
 }
