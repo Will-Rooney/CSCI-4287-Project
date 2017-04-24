2e7e2d947d15cfa462d3071ebc7030f24288ffa9
lia907 <soo.kim@ucdenver.edu>
2017-04-24 15:09:46 -0600

    Update Laser_Sensor.ino
    Updated the left and right sensors and front

--- Laser_Sensor.ino
+++ Laser_Sensor.ino
@@ -8,8 +8,8 @@ extern "C" {
 #define TCAADDR 0x70
 
 Adafruit_VL53L0X front = Adafruit_VL53L0X();
-Adafruit_VL6180X side1 = Adafruit_VL6180X();
-Adafruit_VL6180X side2 = Adafruit_VL6180X();
+Adafruit_VL6180X leftside = Adafruit_VL6180X();
+Adafruit_VL6180X rightside = Adafruit_VL6180X();
 
 void tcaselect(uint8_t i){
   if(i > 7) return;
@@ -28,21 +28,21 @@ void setup() {
   Serial.println("Ready");
 
   //Initializing front sensor (VL53L0X)
-  //On SCL/SDA 5
-  tcaselect(5);
+  //On 4
+  tcaselect(4);
   if(!front.begin()){
     Serial.println("VL53L0X Not Detected");
     while(1);
   }
-  //On 7
-  tcaselect(7);
-  if(!side1.begin()){
+  //On 0
+  tcaselect(0);
+  if(!leftside.begin()){
     Serial.println("Left VL6180X Not Detected");
     while(1);
   }
   //On 2
   tcaselect(2);
-  if(!side2.begin()){
+  if(!rightside.begin()){
     Serial.println("Right VL6180X Not Detected");
     while(1);
   }
@@ -51,10 +51,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   VL53L0X_RangingMeasurementData_t measure;
-  float lux1;
-  float lux2;
+  float luxleft;
+  float luxright;
 
-  tcaselect(5);
+  tcaselect(4);
   front.rangingTest(&measure, false);
   if(measure.RangeStatus != 4){
     Serial.print("Distance (mm): ");
@@ -64,23 +64,23 @@ void loop() {
     Serial.println("Out Of Range");
   }
 
-  tcaselect(7);
-  lux1 = side1.readLux(VL6180X_ALS_GAIN_5);
+  tcaselect(0);
+  luxleft = leftside.readLux(VL6180X_ALS_GAIN_5);
   Serial.print("Left Lux: ");
-  Serial.println(lux1);
-  uint8_t range1 = side1.readRange();
-  uint8_t status1 = side1.readRangeStatus();
+  Serial.println(luxleft);
+  uint8_t range1 = leftside.readRange();
+  uint8_t status1 = leftside.readRangeStatus();
   if(status1 == VL6180X_ERROR_NONE){
     Serial.print("Left Range: ");
     Serial.println(range1);
   }
 
   tcaselect(2);
-  lux2 = side2.readLux(VL6180X_ALS_GAIN_5);
+  luxright = rightside.readLux(VL6180X_ALS_GAIN_5);
   Serial.print("Right Lux: ");
-  Serial.println(lux2);
-  uint8_t range2 = side2.readRange();
-  uint8_t status2 = side2.readRangeStatus();
+  Serial.println(luxright);
+  uint8_t range2 = rightside.readRange();
+  uint8_t status2 = rightside.readRangeStatus();
   if(status2 == VL6180X_ERROR_NONE){
     Serial.print("Right Range: ");
     Serial.println(range2);
