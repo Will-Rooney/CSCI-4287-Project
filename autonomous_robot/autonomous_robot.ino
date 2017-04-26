0717eb92a6531fa620ea0b71024177336607cf74
Will-Rooney <william.rooney@ucdenver.edu>
2017-04-26 14:16:03 -0600

    Update 1 - Integration

--- autonomous_robot/autonomous_robot.ino
+++ autonomous_robot/autonomous_robot.ino
@@ -1,12 +1,70 @@
-/*
-   Name: autonomous_robot.ino
-   Created: 4/25/2017
+/**
+   Sketch for the CSCI 4287 team project, April 2017,
+   a self driving car project.
+
+   Team members:
+      Will Rooney
+      Herman Colato
+      Soo Kim
+      Howard Van Dam
+
+   Hardware components:
+      Arduino Mega
+      Arduino Motor Sheild
+      Adafruit VL6180x
+      Adafruit VL5130X
+      Adafruit HC-SR04
+      Sparkfun MPU9250
 */
 
+#include <Wire.h>
+#include <NewPing.h>
+
+extern "C" {
+#include "utility/twi.h"
+}
+
+#include "Adafruit_VL53L0X.h"
+#include "Adafruit_VL6180X.h"
+#include "MPU9250.h"
+#include "quaternionFilters.h"
+#include "ArduinoMotorShieldR3.h"
 #include "dstar.h"
 
 #define ledPin 13
 
+/**
+    Set this define to 1 to run the sensor/motor checks
+                       0 to skip
+*/
+#define TEST_COMPONENTS 1
+
+/**
+    Constants for sensor addresses and pin assignments
+*/
+
+#define ledPin 13
+#define TCAADDR  0x70    //  multiplexer 
+#define FRONT    0x40
+#define LEFT     0x00
+#define RIGHT    0x20
+#define IMU_ADDR 0x90
+
+#define MAX_I2C_INPUTS 7
+#define TRIGGER_PIN    12
+#define ECHO_PIN       11
+#define MAX_DISTANCE   200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+Adafruit_VL53L0X front = Adafruit_VL53L0X();
+Adafruit_VL6180X leftside = Adafruit_VL6180X();
+Adafruit_VL6180X rightside = Adafruit_VL6180X();
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);   // NewPing setup of pins and maximum distance.
+MPU9250 theIMU;
+ArduinoMotorShieldR3 md;
+
+/**
+     Pathfinding variables and constants
+*/
 DStar search;
 
 struct Vehicle {
@@ -39,10 +97,38 @@ void setup() {
   car.orientation = 1; // RIGHT
 
   /* Sensor & Other Init */
+  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+
+  // Initialize the laser sensors
+  tcaselect(FRONT);
+  if (!front.begin()) {
+    Serial.println("VL53L0X Not Detected");
+  }
+  tcaselect(LEFT);
+  if (!leftside.begin()) {
+    Serial.println("Left VL6180X Not Detected");
+  }
+  tcaselect(RIGHT);
+  if (!rightside.begin()) {
+    Serial.println("Right VL6180X Not Detected");
+  }
+  //  Initialize the Motor Shield
+  md.init();
+
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  if (GOAL) { // GOAL REACHED
+  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+
+  if ( TEST_COMPONENTS ) {
+    Serial.print("Testing Sensors: ");
+    testUltrasonic();
+    testLasers();
+    testMotors();
+    //   testIMU();
+  }
+  else if (GOAL) { // GOAL REACHED
     digitalWrite(ledPin, HIGH);
     delay(1000);
     digitalWrite(ledPin, LOW);
@@ -188,8 +274,8 @@ void loop() {
 }
 
 /* Get reorientation action for the car, based on current action
- * (0 == NO ACTION NEEDED, 1 = TURN LEFT, 2 = TURN RIGHT, 3 = TURN AROUND)
- */
+   (0 == NO ACTION NEEDED, 1 = TURN LEFT, 2 = TURN RIGHT, 3 = TURN AROUND)
+*/
 byte getReorientAct() {
   byte orientation = car.orientation;
   if (orientation == action)
@@ -253,3 +339,198 @@ byte getReorientAct() {
   }
   return 0;
 }
+
+/**
+    Multiplexer Addressing function
+*/
+void tcaselect(uint8_t i) {
+  if (i > MAX_I2C_INPUTS) return;
+
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();
+}
+
+/**
+   Sensor test functions
+*/
+void testUltrasonic() {
+
+  for ( int i = 1; i < 20; i++ )
+  {
+    delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+    Serial.print("Ping: ");
+    Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+    Serial.println("cm");
+    delay(100);
+  }
+}
+
+void testLasers() {
+
+  VL53L0X_RangingMeasurementData_t measure;
+  float luxleft;
+  float luxright;
+  int i = 0;
+
+  tcaselect( FRONT );
+  for ( int i = 1; i < 20; i++ )
+  {
+    front.rangingTest(&measure, false);
+    if (measure.RangeStatus != 4) {
+      Serial.print("Distance (mm): ");
+      Serial.println(measure.RangeMilliMeter);
+    }
+    else {
+      Serial.println("Out Of Range");
+    }
+    delay(50);
+  }  // end for
+
+  tcaselect( LEFT );
+  for ( int i = 1; i < 20; i++ )
+  {
+    luxleft = leftside.readLux(VL6180X_ALS_GAIN_5);
+    Serial.print("Left Lux: ");
+    Serial.println(luxleft);
+    uint8_t range1 = leftside.readRange();
+    uint8_t status1 = leftside.readRangeStatus();
+    if (status1 == VL6180X_ERROR_NONE) {
+      Serial.print("Left Range: ");
+      Serial.println(range1);
+    }
+    delay(50);
+  }  // end for
+
+  tcaselect( RIGHT );
+  for ( int i = 1; i < 20; i++ )
+  {
+    luxright = rightside.readLux(VL6180X_ALS_GAIN_5);
+    Serial.print("Right Lux: ");
+    Serial.println(luxright);
+    uint8_t range2 = rightside.readRange();
+    uint8_t status2 = rightside.readRangeStatus();
+    if (status2 == VL6180X_ERROR_NONE) {
+      Serial.print("Right Range: ");
+      Serial.println(range2);
+    }
+    delay(50);
+  }  // end for
+
+  delay(50);
+}  // end testLasers
+
+void testMotors() {
+
+  Serial.println("M1 Speed 100% Forward");
+  md.setM1Speed(400);
+  Serial.println("M2 Speed 100% Forward");
+  md.setM2Speed(400);
+  Serial.print("M1 current: ");
+  Serial.println(md.getM1CurrentMilliamps());
+  Serial.print("M2 current: ");
+  Serial.println(md.getM2CurrentMilliamps());
+  delay(2000);
+
+  Serial.println("M1 Speed 100% Backward");
+  md.setM1Speed(-400);
+  Serial.println("M2 Speed 100% Backward");
+  md.setM2Speed(-400);
+  Serial.print("M1 current: ");
+  Serial.println(md.getM1CurrentMilliamps());
+  Serial.print("M2 current: ");
+  Serial.println(md.getM2CurrentMilliamps());
+  delay(2000);
+
+  Serial.println("M1 Speed 50% Forward");
+  md.setM1Speed(200);
+  Serial.println("M2 Speed 50% Forward");
+  md.setM2Speed(200);
+  Serial.print("M1 current: ");
+  Serial.println(md.getM1CurrentMilliamps());
+  Serial.print("M2 current: ");
+  Serial.println(md.getM2CurrentMilliamps());
+  delay(2000);
+
+  Serial.println("M1 Speed 50% Backward");
+  md.setM1Speed(-200);
+  Serial.println("M2 Speed 50% Backward");
+  md.setM2Speed(-200);
+  Serial.print("M1 current: ");
+  Serial.println(md.getM1CurrentMilliamps());
+  Serial.print("M2 current: ");
+  Serial.println(md.getM2CurrentMilliamps());
+  delay(2000);
+
+  Serial.println("M1 Speed 0%");
+  md.setM1Speed(0);
+  Serial.println("M2 Speed 0%");
+  md.setM2Speed(0);
+  Serial.print("M1 current: ");
+  Serial.println(md.getM1CurrentMilliamps());
+  Serial.print("M2 current: ");
+  Serial.println(md.getM2CurrentMilliamps());
+  delay(2000);
+
+}  // end testMotors
+
+
+void testIMU() {
+
+  byte c = theIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
+  Serial.print("MPU9250 "); Serial.print("I AM "); Serial.print(c, HEX);
+  Serial.print(" I should be "); Serial.println(0x71, HEX);
+
+  if (c == 0x71) // WHO_AM_I should always be 0x68
+  {
+    Serial.println("MPU9250 is online...");
+
+    // Start by performing self test and reporting values
+    theIMU.MPU9250SelfTest(theIMU.SelfTest);
+    Serial.print("x-axis self test: acceleration trim within : ");
+    Serial.print(theIMU.SelfTest[0], 1); Serial.println("% of factory value");
+    Serial.print("y-axis self test: acceleration trim within : ");
+    Serial.print(theIMU.SelfTest[1], 1); Serial.println("% of factory value");
+    Serial.print("z-axis self test: acceleration trim within : ");
+    Serial.print(theIMU.SelfTest[2], 1); Serial.println("% of factory value");
+    Serial.print("x-axis self test: gyration trim within : ");
+    Serial.print(theIMU.SelfTest[3], 1); Serial.println("% of factory value");
+    Serial.print("y-axis self test: gyration trim within : ");
+    Serial.print(theIMU.SelfTest[4], 1); Serial.println("% of factory value");
+    Serial.print("z-axis self test: gyration trim within : ");
+    Serial.print(theIMU.SelfTest[5], 1); Serial.println("% of factory value");
+
+    // Calibrate gyro and accelerometers, load biases in bias registers
+    theIMU.calibrateMPU9250(theIMU.gyroBias, theIMU.accelBias);
+
+    theIMU.initMPU9250();
+    // Initialize device for active mode read of acclerometer, gyroscope, and
+    // temperature
+    Serial.println("MPU9250 initialized for active data mode....");
+
+    // Read the WHO_AM_I register of the magnetometer, this is a good test of
+    // communication
+    byte d = theIMU.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
+    Serial.print("AK8963 "); Serial.print("I AM "); Serial.print(d, HEX);
+    Serial.print(" I should be "); Serial.println(0x48, HEX);
+
+    // Get magnetometer calibration from AK8963 ROM
+    theIMU.initAK8963(theIMU.magCalibration);
+    // Initialize device for active mode read of magnetometer
+    Serial.println("AK8963 initialized for active data mode....");
+
+    //  Serial.println("Calibration values: ");
+    Serial.print("X-Axis sensitivity adjustment value ");
+    Serial.println(theIMU.magCalibration[0], 2);
+    Serial.print("Y-Axis sensitivity adjustment value ");
+    Serial.println(theIMU.magCalibration[1], 2);
+    Serial.print("Z-Axis sensitivity adjustment value ");
+    Serial.println(theIMU.magCalibration[2], 2);
+
+  } // if (c == 0x71)
+  else
+  {
+    Serial.print("Could not connect to MPU9250: 0x");
+    Serial.println(c, HEX);
+  }
+}  // end test IMU
